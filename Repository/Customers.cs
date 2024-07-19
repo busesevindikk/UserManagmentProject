e206102bad99c0dbf081c2704fb780b850a0773f
Buse Sevindik <busesevindikk@gmail.com>
2024-07-19 14:41:45 +0300

    project directories created

---
+++ Repository/Customers.cs
@@ -0,0 +1,21 @@
+﻿namespace Repository
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnterDate { get; set; }
+
+        public DateTime ExitDate { get; set; }
+
+        public string Gender { get; set; }
+
+
+
+
+
+    }
+}
