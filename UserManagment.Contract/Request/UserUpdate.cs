dee6e8098291a698f4ddaedc2ff734cec8f0cb44
Buse Sevindik <busesevindikk@gmail.com>
2024-07-24 17:01:11 +0300

    Contract layer have been created.

---
+++ UserManagment.Contract/Request/UserUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagment.Contract.Request
+{
+    public class UserUpdate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string surName { get; set; }
+        public string Email { get; set; }
+ 
+   
+    }
+}
