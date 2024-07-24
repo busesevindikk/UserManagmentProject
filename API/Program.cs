9023593bac608ae734d2345d0b650a77e8a5dabf
Buse Sevindik <busesevindikk@gmail.com>
2024-07-24 16:58:19 +0300

    API layer have been created.

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using UseManagment.Business.Mapping;
+using UserManagement.Business;
+using UserManagement.DataAccess;
+using UserManagement.DataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add DbContext to the services
+builder.Services.AddDbContext<UserManagementDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
+// Register the UserService
+builder.Services.AddScoped<IUserService, UserService>();
+
+// Register the UserRepository
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
