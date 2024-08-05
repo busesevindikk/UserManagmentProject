f35cbc5e647229e533c3a4d613d7075a8c54380d
busesevindikk <busevinndikk@gmail.com>
2024-08-05 13:26:25 +0300

    Added WorkerService configuration. Updated folders name.

--- UserManagement.DataAccess/obj/Debug/net8.0/UserManagement.DataAccess.AssemblyInfo.cs
+++ UserManagement.DataAccess/obj/Debug/net8.0/UserManagement.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4a741be1322596bbc462cc107bf7f82fafee29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbeb5b5a12d13b5f980498ac35c6d6c807823643")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WorkerService/obj/Debug/net8.0/WorkerService.AssemblyInfo.cs
+++ WorkerService/obj/Debug/net8.0/WorkerService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4a741be1322596bbc462cc107bf7f82fafee29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbeb5b5a12d13b5f980498ac35c6d6c807823643")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
